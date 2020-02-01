baf8617fb04f981401b75a95a4435ec4ff9643c4
Tanner555 <theman989898@gmail.com>
2020-02-01 09:47:55 -0800

    Added Garbage Collection Test Script

---
+++ Extras/Editor/MyGarbageCollectionTestEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace BaseFramework
+{
+    [CustomEditor(typeof(MyGarbageCollectionTest))]
+    public class MyGarbageCollectionTestEditor : Editor
+    {
+        MyGarbageCollectionTest myGarbageCollectionTest => (MyGarbageCollectionTest)target;
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            if (GUILayout.Button("Clear Ram"))
+            {
+                Debug.Log("Clearing Ram");
+                Resources.UnloadUnusedAssets();
+                UnityEngine.Scripting.GarbageCollector.CollectIncremental(1000000000000);
+            }
+        }
+    }
+}
